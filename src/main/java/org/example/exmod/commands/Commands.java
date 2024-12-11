c1e9ab6eebf440607b713a0c17c7685994e79736
Mr Zombii <thezombiigames@gmail.com>
2024-12-10 21:27:13 -0600

    Update to Cr 0.3.11 and Puzzle 2.1.9, fixed Commands.java

--- src/main/java/org/example/exmod/commands/Commands.java
+++ src/main/java/org/example/exmod/commands/Commands.java
@@ -7,21 +7,22 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import finalforeach.cosmicreach.blocks.BlockState;
+import finalforeach.cosmicreach.blocks.MissingBlockStateResult;
 
 public class Commands {
 
     public static void register() {
         LiteralArgumentBuilder<ServerCommandSource> cmd = CommandManager.literal("setBlock");
-        cmd.then(CommandManager.argument("x", IntegerArgumentType.integer())
-                .then(CommandManager.argument("y", IntegerArgumentType.integer())
-                        .then(CommandManager.argument("z", IntegerArgumentType.integer())
-                                .then(CommandManager.argument("blockstate", StringArgumentType.greedyString())
+        cmd.then(CommandManager.argument(ServerCommandSource.class, "x", IntegerArgumentType.integer())
+                .then(CommandManager.argument(ServerCommandSource.class, "y", IntegerArgumentType.integer())
+                        .then(CommandManager.argument(ServerCommandSource.class, "z", IntegerArgumentType.integer())
+                                .then(CommandManager.argument(ServerCommandSource.class, "blockstate", StringArgumentType.greedyString())
                                         .executes(context -> {
                                             int x = IntegerArgumentType.getInteger(context, "x");
                                             int y = IntegerArgumentType.getInteger(context, "y");
                                             int z = IntegerArgumentType.getInteger(context, "z");
                                             String blockState = StringArgumentType.getString(context, "blockstate");
-                                            BlockUtil.setBlockAt(null, BlockState.getInstance(blockState), x, y, z);
+                                            BlockUtil.setBlockAt(null, BlockState.getInstance(blockState, MissingBlockStateResult.MISSING_OBJECT), x, y, z);
                                             return 0;
                                         })
                                 )
